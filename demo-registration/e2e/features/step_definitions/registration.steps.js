fcd0acdd0f0e92bee6dfda87d15595c88a3dd756
cstanfield <chaley.stanfield@panerabread.com>
2016-09-08 17:59:00 -0400

    Adding registration feature

--- demo-registration/e2e/features/step_definitions/registration.steps.js
+++ demo-registration/e2e/features/step_definitions/registration.steps.js
@@ -8,41 +8,37 @@ var chai = require('chai'),
   domain = '@gmail.com',
   maritalStatus = 'Married',
   hobby = 'Reading',
-  phone = '9125552317';
+  phone = '9125552317',
+  RegistrationPage = require('./registration.page.obj.js'),
+  registrationPage = new RegistrationPage();
 
 module.exports = function () {
-  this.Given(/^I am on the ToolsQA Registration Page$/, function (callback) {
-    //client.url(this.baseUrl);
-    browser.url('http://demoqa.com/registration/');
+  this.Given(/^User is on the ToolsQA HomePage$/, function (callback) {
+    browser.url('/');
     callback();
   });
 
-  this.When(/^I enter text into the search bar$/, function (callback) {
-    browser.setValue('#name_3_firstname', 'Chaley');
-    browser.setValue('#name_3_lastname', text);
-    browser.element('[value="' + maritalStatus.toLowerCase() + '"]').click();
-    browser.element('[value="' + hobby.toLowerCase() + '"]').click();
-    browser.element('#dropdown_7').click();
-    browser.element('[value="' + country + '"]').click();
-    browser.element('#mm_date_8').click();
-    browser.element('[value="7"]').click();
-    browser.element('#dd_date_8').click();
-    browser.element('[value="23"]').click();
-    browser.element('#yy_date_8').click();
-    browser.element('[value="1993"]').click();
-    browser.setValue('#phone_9', phone);
-    browser.setValue('#username', text + timestamp);
-    browser.setValue('#email_1', text + '+' + timestamp + domain);
-    browser.setValue('#description', text);
-    browser.setValue('#password_2', text);
-    browser.setValue('#confirm_password_password_2', text);
-    browser.element('[type="submit"]').click();
+  this.When(/^Navigates to the registration page$/, function (callback) {
+    registrationPage.registrationLink();
     callback();
   });
 
-  this.Then(/^I should see results$/, function (callback) {
-    //browser.element('.piereg_message').waitForExist(5000);
-    expect(browser.element('.piereg_message').getText()).to.equal('Thank you for your registration');
+  this.When(/^Enters (.*) information into the form$/, function (isValid, callback) {
+    registrationPage.enterFormInfo(isValid);
+    callback();
+  });
+
+  this.When(/^Clicks the Submit button$/, function (callback) {
+    registrationPage.submit();
+    callback();
+  });
+
+  this.Then(/^User will receive message indicating registration was (.*)$/, function (result, callback) {
+    if (result.indexOf('unsuccessful') !== -1) {
+      expect(browser.element('.legend.error').getText()).to.equal('* Invalid email address');
+    } else {
+      expect(browser.element('.piereg_message').getText()).to.equal('Thank you for your registration');
+    }
     callback();
   });
 };
